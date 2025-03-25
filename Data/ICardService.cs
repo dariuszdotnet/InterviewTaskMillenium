18cc42d72f2d5734bfb935496b82921a54a8a908
your name <you@example.com>
2025-03-25 15:25:58 +0100

    add project responsible for delivering data related to cards

---
+++ Data/ICardService.cs
@@ -0,0 +1,18 @@
+﻿using Common;
+
+namespace Data
+{
+    /// <summary>
+    /// Service responsible for creation of cards as well as retrieving its details.
+    /// </summary>
+    public interface ICardService
+    {
+        /// <summary>
+        /// Retrieving card details from a service.
+        /// </summary>
+        /// <param name="userId">User identifier in a format <c>User1</c>, <c>User2</c> etc.</param>
+        /// <param name="cardNumber">Card indentifier in a format <c>Card11</c>, <c>Card12</c>, <c>Card22</c> etc.</param>
+        /// <returns>Details related to specific card as an object of type <see cref="CardDetails"/></returns>
+        Task<CardDetails?> GetCardDetails(string userId, string cardNumber);
+    }
+}
